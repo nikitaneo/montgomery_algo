f45fff398e2047904a95b80e1712e14ccad49e73
Nikita <sergeevniks@ya.ru>
2019-01-06 23:23:29 +0300

    Added gtest

--- test/test.cu
+++ test/test.cu
@@ -6,77 +6,68 @@
 #include <iostream>
 #include <chrono>
 
-TEST(FastMultCPU, Simple)
+class FastMultTest : public ::testing::Test
 {
-    unsigned size = 128 * 1024;
-
-    std::vector<uint128_t> a( size );
-    std::vector<uint128_t> b( size );
-    std::vector<uint128_t> c( size );
-    std::vector<uint128_t> naive_c( size );
-    
-    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
-	std::mt19937_64 gen( seed );
-	std::uniform_int_distribution<uint64_t> dist( 1, uint64_t(-1) );
-	for( unsigned i = 0; i < size; i++ )
+protected:
+    void SetUp() override
     {
-        a[i] = dist( gen );
-		b[i] = dist( gen );
+        a = std::vector<uint128_t>( size );
+        b = std::vector<uint128_t>( size );
+        c = std::vector<uint128_t>( size );
+
+        unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+        std::mt19937_64 gen( seed );
+        std::uniform_int_distribution<uint64_t> dist( 1, uint64_t(-1) );
+        for( unsigned i = 0; i < size; i++ )
+        {
+            a[i] = dist( gen );
+            b[i] = dist( gen );
+        }
+        
+        for( unsigned i = 0; i < c.size(); i++ )
+        {
+            c[i] = a[i] * b[i] % mod;
+        }
     }
-    uint128_t mod(14797879391564958223ULL);
+
+    unsigned size{ 128 * 1024 };
+    uint128_t mod{ 14797879391564958223ULL };
+    std::vector<uint128_t> a;
+    std::vector<uint128_t> b;
+    std::vector<uint128_t> c;
+};
+
+TEST_F(FastMultTest, CPU)
+{
+    std::vector<uint128_t> fast_c( size );
 
     auto begin = std::chrono::steady_clock::now();
-    modmul_cpu( a.data(), b.data(), a.size(), mod, c.data() );
+    modmul_cpu( a.data(), b.data(), a.size(), mod, fast_c.data() );
     auto end = std::chrono::steady_clock::now();
     auto el_time = std::chrono::duration_cast<std::chrono::microseconds>( end - begin ).count();
 
     std::cerr << "Montgomery multiplication (CPU) on " << size << " elements has finished with elapsed time " << el_time << " ms." << std::endl;
 
-    for( unsigned i = 0; i < naive_c.size(); i++ )
-    {
-        naive_c[i] = a[i] * b[i] % mod;
-    }
-
     for( unsigned i = 0; i < c.size(); i++ )
     {
-        ASSERT_EQ(c[i], naive_c[i]);
+        ASSERT_EQ(c[i], fast_c[i]);
     }
 }
 
-TEST(FastMultGPU, Simple)
+TEST_F(FastMultTest, GPU)
 {
-    unsigned size = 128 * 1024;
-
-    std::vector<uint128_t> a( size );
-    std::vector<uint128_t> b( size );
-    std::vector<uint128_t> c( size );
-    std::vector<uint128_t> naive_c( size );
-    
-    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
-	std::mt19937_64 gen( seed );
-	std::uniform_int_distribution<uint64_t> dist( 1, uint64_t(-1) );
-	for( unsigned i = 0; i < size; i++ )
-    {
-        a[i] = dist( gen );
-		b[i] = dist( gen );
-    }
-    uint128_t mod = 5;
+    std::vector<uint128_t> fast_c( size );
 
     auto begin = std::chrono::steady_clock::now();
-    modmul_gpu( a.data(), b.data(), a.size(), mod, c.data() );
+    modmul_gpu( a.data(), b.data(), a.size(), mod, fast_c.data() );
     auto end = std::chrono::steady_clock::now();
     auto el_time = std::chrono::duration_cast<std::chrono::microseconds>( end - begin ).count();
 
     std::cerr << "Montgomery multiplication (GPU) on " << size << " elements has finished with elapsed time " << el_time << " ms." << std::endl;
 
-    for( unsigned i = 0; i < naive_c.size(); i++ )
-    {
-        naive_c[i] = a[i] * b[i] % mod;
-    }
-
     for( unsigned i = 0; i < c.size(); i++ )
     {
-        ASSERT_EQ(c[i], naive_c[i]);
+        ASSERT_EQ(c[i], fast_c[i]);
     }
 }
 
